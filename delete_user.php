14ce04ae1bbfdd6574d6ca36ea63268ecc50c2e3
azraadawood <azu.dawood24@gmail.com>
2024-06-20 13:48:05 +0200

    delete_user.php

--- delete_user.php
+++ delete_user.php
@@ -1,23 +1,24 @@
-
-<?php //PHP code
+<?php
+// Include configuration and sidebar files
 include '../config.php';
 include 'sidebar.php';
 
-
+// Check if user ID is provided in the URL
 if (isset($_GET['id'])) {
     $user_id = $_GET['id'];
 
+    // Prepare SQL statement to delete user with specified ID
     $stmt = $conn->prepare("DELETE FROM users WHERE id = ?");
     $stmt->bind_param("i", $user_id);
 
-    
+    // Execute the deletion query
     if ($stmt->execute()) {
         echo "User was deleted successfully.";
     } else {
         echo "Error while deleting user: " . $conn->error;
     }
 
-   
+    // Redirect to display_accounts.php after deletion
     header("Location: display_accounts.php");
     exit;
 } else {
