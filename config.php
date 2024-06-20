66afd99c950597aa6624ccde501948d84e4d4006
azraadawood <azu.dawood24@gmail.com>
2024-06-20 13:54:24 +0200

    config.php

--- config.php
+++ config.php
@@ -1,11 +1,16 @@
 <?php //PHP code
 //Connection to database
-	$conn = new mysqli("localhost","root","","amourdatabase");
-	if($conn->connect_error){
-		die("Connection Failed!".$conn->connect_error);
-	}
-?>
 
+$servername = "localhost"; //database server
+$username = "amourpnc_amourdatabase"; //username
+$pass = "Welcome@2024"; //password
+$dbname = "amourpnc_amourdatabase1"; //database name
+
+$conn = mysqli_connect($servername, $username, $pass, $dbname);
+
+
+
+?>
 
 
 
